b59be9fbb4692f7c2d79b9aa81af4eec7d5dedae
subarna-ghosh <subarnaghosh371@gmail.com>
2025-06-30 22:39:19 +0530

    did some updation

--- js/index.js
+++ js/index.js
@@ -8,26 +8,6 @@ window.addEventListener("DOMContentLoaded", () => {
   AOS.init();
 });
 
-// Cursor Js Implementation
-
-const cursor = document.querySelector(".cursor");
-let pos = { x: 0, y: 0 };
-let cursorPos = { x: 0, y: 0 };
-const easting = 8;
-
-window.addEventListener("mousemove", (e) => {
-  pos.x = e.clientX;
-  pos.y = e.clientY;
-});
-
-function loop() {
-  cursorPos.x += (pos.x - cursorPos.x) / easting;
-  cursorPos.y += (pos.y - cursorPos.y) / easting;
-  cursor.style.transform = `translate(${cursorPos.x}px, ${cursorPos.y}px)`;
-  requestAnimationFrame(loop);
-}
-
-requestAnimationFrame(loop);
 
 // Navbar Section
 const navToggler = document.querySelector(".nav-toggler");
